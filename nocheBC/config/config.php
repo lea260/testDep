ba5aeea9ab7bf0bc28755a74cc4be7f671f44842
Leandro <lcalvetti1@gmail.com>
2022-11-11 01:09:56 -0300

    quito nocheBC

--- nocheBC/config/config.php
+++ nocheBC/config/config.php
@@ -1,7 +1,7 @@
 <?php
 
 //define('URL', 'http://localhost/mvc/');
-define('URL', 'https://' . $_SERVER['HTTP_HOST'] . '/nocheBC/');
+define('URL', 'https://' . $_SERVER['HTTP_HOST'] . '/');
 
 //conexion a la base de datos
 define('HOST', 'localhost');
